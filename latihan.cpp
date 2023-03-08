0ba693c7a9e632ccae700fe6ea9d24dd3bd39fc1
KsatrioDananjaya <ksatrio.dana.ft20@mail.umy.ac.id>
2023-03-08 16:06:07 +0700

    first commit

---
+++ latihan.cpp
@@ -0,0 +1,14 @@
+#include <iostream> //File yang disertakan
+using namespace std;
+
+int main(){ //begin
+
+    double Luas, L, P; //numeric
+    cout << "Masukkan Panjangnya : ";
+    cin >> P; //kode untuk Accept P
+    cout << "Masukkan Lebarnya : ";
+    cin >> L;
+    Luas = P*L; //kode untuk Compute
+    cout << "Luasnya : " << Luas;  
+
+}
